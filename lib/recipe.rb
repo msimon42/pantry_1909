e67ce965b11b764d4e8ec1a3a755d41b3e3af743
msimon42 <msimon@uab.edu>
2019-11-05 09:32:29 -0700

    Add add ingredients method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,11 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name, :ingredients_required
   def initialize(name)
     @name = name
     @ingredients_required = Hash.new
   end
 
+  def add_ingredient(ingredient, amt)
+    @ingredients_required[ingredient] = amt
+  end
 end
