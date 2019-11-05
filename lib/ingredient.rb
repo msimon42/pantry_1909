25eb6d2ffe9e7dce9717061f4e6dc29056a206bd
msimon42 <msimon@uab.edu>
2019-11-05 09:13:26 -0700

    Add instance vars

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,4 +1,10 @@
-class Ingridient
+class Ingredient
+  attr_reader :name, :unit, :calories
+  def initialize(name, unit, calories)
+    @name = name
+    @unit = unit
+    @calories = calories
+  end
 
 
-end   
+end
