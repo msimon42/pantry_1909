bccd72aed3c2a58b9e40563a5d0b4458b0dc8524
msimon42 <msimon@uab.edu>
2019-11-05 10:05:03 -0700

    Add test for stock check

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,6 +1,8 @@
 require 'minitest'
 require 'minitest/autorun'
 require_relative '../lib/pantry'
+require_relative '../lib/ingredient'
+require_relative '../lib/recipe'
 
 class PantryTest < Minitest::Test
   def setup
@@ -8,11 +10,19 @@ class PantryTest < Minitest::Test
     @cheese = Ingredient.new("Cheese", "C", 50)
     @mac = Ingredient.new("Macaroni", "oz", 200)
     @mac_and_cheese = Recipe.new("Mac and Cheese")
-    @mac_and_cheese.add_ingredient(cheese, 2)
-    @mac_and_cheese.add_ingredient(mac, 8)
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
   end
 
   def test_existence
     assert_instance_of Pantry, @pantry
-  end   
+  end
+
+  def test_stock
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_stock_check
+    assert_equal 0, @pantry.stock_check(@cheese)
+  end
 end
