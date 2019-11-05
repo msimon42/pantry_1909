4744245296116541ae07d9cc1d4e5f05bb9b92b4
msimon42 <msimon@uab.edu>
2019-11-05 09:59:21 -0700

    Add pantry test

---
+++ test/pantry_test.rb
@@ -0,0 +1,14 @@
+require 'minitest'
+require 'minitest/autorun'
+require_relative '../lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @cheese = Ingredient.new("Cheese", "C", 50)
+    @mac = Ingredient.new("Macaroni", "oz", 200)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+    @mac_and_cheese.add_ingredient(cheese, 2)
+    @mac_and_cheese.add_ingredient(mac, 8)
+  end   
+end
