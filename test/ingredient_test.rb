7b613f748d4d55a8b670499bc6bf38de771e2c66
msimon42 <msimon@uab.edu>
2019-11-05 09:11:52 -0700

    Add setup

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -1,3 +1,10 @@
 require 'minitest'
 require 'minitest/autorun'
 require_relative '../lib/ingredient'
+
+class IngredientTest < Minitest::Test
+  def setup
+    @cheese = Ingredient.new('Cheese', 'oz', 50)
+  end   
+
+end
