6f94ab9a6406e7844097c57afa8b137e157e55c4
msimon42 <msimon@uab.edu>
2019-11-05 11:18:07 -0700

    Add summary method

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -1,4 +1,28 @@
+require 'pry'
 class CookBook
+  attr_reader :recipes
+  def initialize
+    @recipes = Array.new
+  end
 
+  def add_recipe(recipe)
+    @recipes << recipe
+  end
 
-end   
+  def summary
+    output = @recipes.map do |recipe|
+      info = Hash.new
+      info[:name] = recipe.name
+      info[:details] = Hash.new
+      info[:details][:ingredients] = recipe.ingredients_required.map do |ingredient|
+        ing_info = Hash.new
+        ing_info[:ingredient] = ingredient[0].name
+        ing_info[:amount] = "#{ingredient[1].to_s} #{ingredient[0].unit}"
+        ing_info
+      end
+      info[:details][:total_calories] = recipe.total_calories
+      info
+    end
+    output
+  end
+end
